740860a400495607867010625cbf76115350e1ff
Edwin Rodriguez <Edwin Rodriguez>
2016-09-27 21:15:39 -0400

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,28 @@ CREATE TABLE teachers
 	name varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
 	PRIMARY KEY (id)
-	
+);
+
+CREATE TABLE students
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE class
+(
+	id int NOT NULL AUTO_INCREMENT,
+	teacher_name varchar(255) NOT NULL,
+	student_name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE assignments
+(
+	id int NOT NULL AUTO_INCREMENT,
+	writing varchar(20000) NOT NULL,
+	grade varchar(2) NOT NULL,
+	PRIMARY KEY (id)
 );

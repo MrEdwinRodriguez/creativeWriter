afc01cd1284af9cd7559976d97807247782836e4
Edwin Rodriguez <Edwin Rodriguez>
2016-09-29 21:12:14 -0400

    started the controller

--- controllers/writer_controller.js
+++ controllers/writer_controller.js
@@ -12,12 +12,11 @@ router.get('/teacherlogin', function(req,res) {
 });
 
 
-
-router.get('/burgers', function(req,res) {
+router.get('/selectteacher', function(req,res) {
 	//express callback response by calling burger.selectAllBurger
-	burger.all(function(burger_data){
+	writer.selectTeacher(function(teacher){
 		//wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-		res.render('index', {burger_data});
+		res.redirect('/studentview');
 	});
 });
 

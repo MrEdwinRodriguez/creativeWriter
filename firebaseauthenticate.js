76b7b9ac695ae764b49576d3da87a97ccfb6adff
MrEdwinRodriguez <mredwinrodriguez@gmail.com>
2016-10-01 13:45:15 -0400

    firebase auth

---
+++ firebaseauthenticate.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.4.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDCLU0IiA8J_-bRcHwOdEK8qIIEqLkKf3s",
+    authDomain: "creativewriter-d899e.firebaseapp.com",
+    databaseURL: "https://creativewriter-d899e.firebaseio.com",
+    storageBucket: "creativewriter-d899e.appspot.com",
+    messagingSenderId: "1038064048502"
+  };
+  firebase.initializeApp(config);
+</script>

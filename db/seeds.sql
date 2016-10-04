5d646fe0a2d9355182ecc64dcdb1457a57891a0a
olewis27 <omar.lewis@flhosp.org>
2016-10-04 19:44:42 -0400

    updated schema & seeds sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,52 @@
 use creativewriter_db;
 
-INSERT INTO teachers (name,email),
+INSERT INTO Mentors (name,email),
 VALUES ('Ms. Parker','Hey_Ms_Parker@hotmail.com');
 VALUES ('Ms. Jackson','Sorry_Ms_Jackson@hotmail.com');
 VALUES ('Mr. Peter' , 'PeteyPablo@UCF.com')
 
-
-INSERT INTO students(name,email),
+INSERT INTO users (teacher_id, student_id, name),
 VALUES ('Edwin Rodriguez','MrPimp@hotmail.com');
 VALUES ('Jason Perry','DaTruth@gmail.com');
 VALUES ('Josh Miller','Slapmamas@aol.com');
 
+INSERT INTO students (name,email,submission),
+VALUES ('Edwin Rodriguez','MrPimp@hotmail.com');
+VALUES ('Jason Perry','DaTruth@gmail.com');
+VALUES ('Josh Miller','Slapmamas@aol.com');
 
-INSERT INTO class (teacher_id, student_id, name)
+INSERT INTO assignments (name,category,random,timer,complexity,description),
+VALUES ('assign# 1','Eco-friendly','_','10:00 mins', student writing);
+VALUES ('assign# 2','Social Network','_','10:00 mins', student writing);
+VALUES ('assign# 3','Geography','_', '10:00 mins', student writing);
+ -- 1- Category - Write about going green or an environmental concern you have.
+ -- 2- Category - Visit your favorite Social Networking website (ie: Facebook, Pinterest, Google, Twitter, etc.) and write a about a post you see there.
+ -- 3- Category - Pick a state or country you’ve never visited. Write about why you would or would not like to visit that place.
+
+ INSERT INTO assignments_has_Submission (assignments_Topics INT, Submission_student_submission INT),
+ VALUES ('Edwin Rodriguez','MrPimp@hotmail.com');
+ VALUES ('Jason Perry','DaTruth@gmail.com');
+ VALUES ('Josh Miller','Slapmamas@aol.com');
+
+ INSERT INTO submission (student_input),
+ VALUES (student_info);
+ VALUES (student_info);
+ VALUES (student_info);
+
+ INSERT INTO Submission_has_student (Submission_student_submission INT, students_students_id INT),
+ VALUES ('Edwin Rodriguez','MrPimp@hotmail.com');
+ VALUES ('Jason Perry','DaTruth@gmail.com');
+ VALUES ('Josh Miller','Slapmamas@aol.com');
+
+ INSERT INTO comments (mentor_id,submission_id,mentor_input,student_input,date/time),
+ VALUES ('Edwin Rodriguez','MrPimp@hotmail.com');
+ VALUES ('Jason Perry','DaTruth@gmail.com');
+ VALUES ('Josh Miller','Slapmamas@aol.com');
+
+ INSERT INTO comments_has_submission (comments_mentor_comment INT, Submission_student_submission INT),
+ VALUES ('Edwin Rodriguez','MrPimp@hotmail.com');
+ VALUES ('Jason Perry','DaTruth@gmail.com');
+ VALUES ('Josh Miller','Slapmamas@aol.com');
 
 VALUES
 (1,1,'Writing 101'),

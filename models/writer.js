0106227f24b0df7bd60be4830754eb9a1d8bc2e6
MrEdwinRodriguez <mredwinrodriguez@gmail.com>
2016-10-11 21:17:26 -0400

    update on models

--- models/writer.js
+++ models/writer.js
@@ -10,21 +10,21 @@ var writer = {
 	},
 
 	selectSubmission: function(cb) {
-		orm.selectSubmission('submission', function(res){
+		orm.all('submission', function(res){
 			cb(res);
 		});
 	},	
 
 	insertUsers: function(name, email, type, cb) {
-		orm.insertUsers('users', ['name', 'email', 'type'], [name, email, type], cb);	
+		orm.create('users', ['name', 'email', 'type'], [name, email, type], cb);	
 	},
 
 	insertSubmission: function(student, text, cb) {
-		orm.insertSubmission('submission', ['user_id', 'user_input'], [student, text], cb);	
+		orm.create('submission', ['user_id', 'user_input'], [student, text], cb);	
 	},
 
 	insertComments: function(mentor, submission, comment, cb) {
-		orm.insertComments('comments', ['user_id', 'submission_id', 'comments'], [mentor, submission, comment], cb);	
+		orm.create('comments', ['user_id', 'submission_id', 'comments'], [mentor, submission, comment], cb);	
 	}	
 
 }

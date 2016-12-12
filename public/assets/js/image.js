528ef99448cc70afa43ab03b8969ec257ab742ed
MrEdwinRodriguez <mredwinrodriguez@gmail.com>
2016-12-11 22:13:18 -0500

    fixed image path

--- public/assets/js/image.js
+++ public/assets/js/image.js
@@ -1,4 +1,4 @@
-var imagesArray = ["assets/img/sky.jpg", "assets/img/vr.jpg", "assets/img/meeting.jpg", "assets/img/fox.jpg", "assets/img/tech.jpg", "assets/img/space.jpg", "assets/img/traffic.png", "assets/img/works.jpg", "assets/img/war.jpg", "assets/img/ocean.jpeg"];
+var imagesArray = ["../assets/img/sky.jpg", "../assets/img/vr.jpg", "../assets/img/meeting.jpg", "../assets/img/fox.jpg", "../assets/img/tech.jpg", "../assets/img/space.jpg", "../assets/img/traffic.png", "../assets/img/works.jpg", "../assets/img/war.jpg", "../assets/img/ocean.jpeg"];
 
 
 //create a function named displayImage
